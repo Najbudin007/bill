555a2e1e10dd00f96828b8617f546d65a61be836
najbudin007 <najbudinalam55@gmail.com>
2022-12-05 15:15:16 +0545

    added migrattions for personal Details

---
+++ app/Http/Controllers/TaxablePersonDetailController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\taxablePersonDetail;
+use Illuminate\Http\Request;
+
+class TaxablePersonDetailController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\taxablePersonDetail  $taxablePersonDetail
+     * @return \Illuminate\Http\Response
+     */
+    public function show(taxablePersonDetail $taxablePersonDetail)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\taxablePersonDetail  $taxablePersonDetail
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(taxablePersonDetail $taxablePersonDetail)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\taxablePersonDetail  $taxablePersonDetail
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, taxablePersonDetail $taxablePersonDetail)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\taxablePersonDetail  $taxablePersonDetail
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(taxablePersonDetail $taxablePersonDetail)
+    {
+        //
+    }
+}
