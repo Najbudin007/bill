dc17082a54f032c9e158b5dc7687ccd461e39b02
najbudin007 <najbudinalam55@gmail.com>
2022-12-29 22:52:31 +0545

    stored data in jaggadarta form

--- app/Http/Controllers/JaggadartaController.php
+++ app/Http/Controllers/JaggadartaController.php
@@ -35,7 +35,46 @@ class JaggadartaController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $request->validate([
+            'gaupalika' => 'required',
+            'ward_no' => 'required',
+        ]);
+
+        $data =  new jaggadarta();
+
+        $data->gaupalika = $request->gaupalika;
+        $data->ward_no = $request->ward_no;
+        $data->p_ward_no = $request->p_ward_no;
+        $data->kitta_no = $request->kitta_no;
+        $data->moth_no = $request->moth_no;
+        $data->sit_no = $request->sit_no;
+        $data->naskha_no = $request->naskha_no;
+        $data->place = $request->place;
+        $data->quality = $request->quality;
+        $data->nature = $request->nature;
+        $data->road = $request->road;
+        $data->road_quality = $request->road_quality;
+        $data->prapti_type = $request->prapti_type;
+        $data->prapta_date = $request->prapta_date;
+        $data->kharid_mulya = $request->kharid_mulya;
+        $data->prati_ikai_mulya = $request->prati_ikai_mulya;
+        $data->under_mulyankan = $request->under_mulyankan;
+        $data->natural_disaster = $request->natural_disaster;
+        $data->mulyankan_rate = $request->mulyankan_rate;
+        $data->malpot_rate = $request->malpot_rate;
+        $data->jagga_mapan_ikai = $request->jagga_mapan_ikai;
+        // Land Details
+        $data->bigha = $request->bigha;
+        $data->kattha = $request->kattha;
+        $data->dhur = $request->dhur;
+        $data->kanwa = $request->road;
+        $data->kanwain = $request->kanwain;
+        $data->hector = $request->hector;
+        $data->acer = $request->acer;
+        $data->meter = $request->meter;
+
+        $data->save();
+        return redirect()->route('karasuli.create');
     }
 
     /**
--- database/migrations/2022_12_22_103351_create_jaggadartas_table.php
+++ database/migrations/2022_12_22_103351_create_jaggadartas_table.php
@@ -15,6 +15,38 @@ return new class extends Migration
     {
         Schema::create('jaggadartas', function (Blueprint $table) {
             $table->id();
+            $table->string('gaupalika')->nullable();
+            $table->string('ward_no')->nullable();
+            $table->string('p_ward_no')->nullable();
+            $table->string('kitta_no')->nullable();
+            $table->string('moth_no')->nullable();
+            $table->string('sit_no')->nullable();
+            $table->string('naskha_no')->nullable();
+            $table->string('place')->nullable();
+            $table->string('quality')->nullable();
+            $table->string('nature')->nullable();
+            $table->string('road')->nullable();
+            $table->string('road_quality')->nullable();
+            $table->string('prapti_type')->nullable();
+            $table->string('prapti_date')->nullable();
+            $table->string('prapta_date')->nullable();
+            $table->string('kharid_mulya')->nullable();
+            $table->string('prati_ikai_mulya')->nullable();
+            $table->string('under_mulyankan')->nullable();
+            $table->string('natural_disaster')->nullable();
+            $table->string('mulyankan_rate')->nullable();
+            $table->string('malpot_rate')->nullable();
+            $table->string('jagga_mapan_ikai')->nullable();
+            // landDetails
+            $table->string('bigha')->nullable()->comment('land Area');
+            $table->string('kattha')->nullable()->comment('land Area');
+            $table->string('dhur')->nullable()->comment('land Area');
+            $table->string('kanwa')->nullable()->comment('land Area');
+            $table->string('kanwain')->nullable()->comment('land Area');
+            $table->string('hector')->nullable()->comment('land Area');
+            $table->string('acer')->nullable()->comment('land Area');
+            $table->string('meter')->nullable()->comment('land Area');
+
             $table->timestamps();
         });
     }
