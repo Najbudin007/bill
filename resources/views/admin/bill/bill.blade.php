eb53c0549126cbcdaca8d89ac73f03dd3cbd30b9
najbudin007 <najbudinalam55@gmail.com>
2023-01-07 09:59:14 +0545

    added bill demo

---
+++ resources/views/admin/bill/bill.blade.php
@@ -0,0 +1,133 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Bill</title>
+    <link rel="stylesheet" href="{{ asset('css/bill.css') }}">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous">
+    </script>
+    <link href="https://cdn.jsdelivr.net/npm/remixicon@2.5.0/fonts/remixicon.css" rel="stylesheet">
+</head>
+
+<body>
+    <div class="container border my-5 p-3">
+        <div class="header ">
+            <div class="logo">
+                <img src="{{ asset('images/sar.png') }}" alt="">
+            </div>
+            <div class="center">
+                <p>इशनाथ नगरपालिका</p>
+                <p>इशनाथ नगरपालिका</p>
+                <p>रौतहट, मधेश प्रदेश, नेपाल</p>
+                <p class="title">Wealth and land bill (asdasd dasd)</p>
+                <p>रौतहट, मधेश प्रदेश, नेपा4</p>
+            </div>
+        </div>
+        <div class="belowheader mt-2">
+            <p>Bidhutiya kar ko sanket num: 1234567890oijhgfe567esrdfg775567</p>
+        </div>
+        <div class="row">
+            <div class="col-md-4">
+                <p>rasid no : 123</p>
+                <p>miti : 2020/02/10</p>
+                <p>kardata ko name thar : 123</p>
+                <p>N.P/G.P : इशनाथ नगरपालिका</p>
+                <p>ghar no : </p>
+            </div>
+            <div class="col-md-4">
+                <br>
+                <p>AA.OO : 2020/2020</p>
+                <p>Pradesh : sdfsdfsd</p>
+                <p>Ward No : sdfsdfsd</p>
+                <p>Kar Data sanket : 00-8-8000</p>
+            </div>
+            <div class="col-md-4">
+                <p>paxilo patak tireko num : </p>
+                <br>
+                <p>District : dasda</p>
+                <p>Sadak/tolko name : dasda</p>
+                <p>Antarik sanket : 2222</p>
+            </div>
+        </div>
+        <center class="text-bold">(Jagga mapan ekaai adasda dasdasd asdasd asdasda dasdasd)</center>
+        <div class="tables mt-2">
+            <div class="titles">
+                <div class="bordbox">Jagga ko biwaran</div>
+                <div>Vautik samarachana ko biwaran</div>
+            </div>
+            <div class="content">
+                <div class="bordbox box1">Sabik GBSSD</div>
+                <div class="bordbox box2">Woda No
+                    <div class="d-flex h-100">
+                        <div class="v-border">sabik</div>
+                        <div class="v-border">hal</div>
+                    </div>
+                </div>
+                <div class="bordbox box3">Naksha No</div>
+                <div class="bordbox box4">Kar xetra</div>
+                <div class="bordbox box5">Kitta No.</div>
+                <div class="bordbox box6">Jamma xetra fal</div>
+                <div class="bordbox box7">Kar Yogya mulya</div>
+                <div class="bordbox box8">Sadak ko name</div>
+                <div class="bordbox box9">Ghar no</div>
+                <div class="bordbox box10">banawot ko kisim</div>
+                <div class="bordbox box11">prakar</div>
+                <div class="bordbox box12">prayog</div>
+                <div class="bordbox box13">xetrafal w.k</div>
+                <div class="bordbox box14">kamyan mulya</div>
+                <div class="bordbox box15">sampati ko jamma mulya</div>
+            </div>
+            <div class="titles">
+                <div>Vumi kar</div>
+            </div>
+            <div class="content">
+                <div class="bordbox box1">Sabik GBSSD</div>
+                <div class="bordbox box2">
+                    <div class="d-flex" style="height: 100%">
+                        <div class="v-border1">sabik</div>
+                        <div class="v-border1">hal</div>
+                    </div>
+                </div>
+                <div class="bordbox box3">Naksha No</div>
+                <div class="bordbox box4">Kar xetra</div>
+                <div class="bordbox box5">Kitta No.</div>
+                <div class="bordbox box6">Jamma xetra fal</div>
+                <div class="bordbox box7">Kar Yogya mulya</div>
+                <div class="bordbox box8">Sadak ko name</div>
+                <div class="bordbox box9">Ghar no</div>
+                <div class="bordbox box10">banawot ko kisim</div>
+                <div class="bordbox box11">prakar</div>
+                <div class="bordbox box12">prayog</div>
+                <div class="bordbox box13">xetrafal w.k</div>
+                <div class="bordbox box14">kamyan mulya</div>
+                <div class="bordbox box15">sampati ko jamma mulya</div>
+            </div>
+            <div class="content">
+                <div class=" box1"></div>
+                <div class=" box2">
+
+                </div>
+                <div class=" box3"></div>
+                <div class="bordbox box4" style="height: 30px">Jamma</div>
+                <div class="bordbox box5" style="width:160px !important">2-0-0-3</div>
+                <div class="bordbox box7">0|00</div>
+                <div class=" box8"></div>
+                <div class=" box9"></div>
+                <div class=" box10"></div>
+                <div class=" box11"></div>
+                <div class=" box12"></div>
+                <div class=" box13"></div>
+                <div class="bordbox box14"></div>
+                <div class="bordbox box15">0|00</div>
+            </div>
+        </div>
+    </div>
+</body>
+
+</html>
